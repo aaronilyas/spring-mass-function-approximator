649b85ae3852d774253c67a9c98b620d7f1f031e
aaronilyas <aaronilyas@hotmail.com>
2026-09-23 11:44:47 -0500

    Setup function to collect values from user in main.py.

--- main.py
+++ main.py
@@ -3,13 +3,38 @@ import torch
 import matplotlib.pyplot as plt
 
 
+def collect_info_from_user_to_train_model(model: MLP) -> None:
+    epochs = int(input("Enter the number of training epochs: "))
+    spring_constant = float(input("Enter the spring constant: "))
+    mass = float(input("Enter the mass: "))
+    damping_coefficient = float(input("Enter the damping coefficient: "))
+    initial_position = float(input("Enter the initial position: "))
+    initial_velocity = float(input("Enter the initial velocity: "))
+
+    model.train_network(
+        epochs=epochs,
+        spring_constant=spring_constant,
+        mass=mass,
+        damping_coefficient=damping_coefficient,
+        inital_condition_position=torch.tensor(initial_position).float(),
+        inital_condition_velocity=torch.tensor(initial_velocity).float(),
+    )
+
+
 def main():
     model = MLP()
-    model.train_network(20, 2, 4, 16, torch.tensor(2), torch.tensor(2))
-    positions = model.predict_position_over_interval_of_time(0, 10)
-    times = list(positions.keys())
-    positions = list(positions.values())
-    plt.scatter(times, positions)
+    collect_info_from_user_to_train_model(model)
+
+    starting_time = input("Enter: the time you wish the graph to start at ")
+    ending_time = input("Enter: the time you wish the graph to end at ")
+    position_at_various_time_values = model.predict_position_over_interval_of_time(
+        int(starting_time), int(ending_time)
+    )
+
+    keys = list(position_at_various_time_values.keys())
+    values = list(position_at_various_time_values.values())
+
+    plt.scatter(keys, values)
     plt.show()
 
 
